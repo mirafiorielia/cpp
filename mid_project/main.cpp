b3e9e60382a072376e353603930b7d0b054b0b29
Mirafiori Elia <el.mirafiori@gmail.com>
2022-11-20 14:21:12 +0100

    - made tets env

--- mid_project/main.cpp
+++ mid_project/main.cpp
@@ -12,8 +12,8 @@ int main() {
     Book book4("Davide", "Mirafiori", "C++ fatto easy", "a88-521-837-E");   // should give invalid isbn
     Book book5("Alberto", "Mirafiori", "C++ fatto easy", "988-521-837-+");  // should give invalid isbn
 
-    Book book6("Elia", "Mirafiori", "C++ fatto easy", "888-521-321-E");  // test borrow and return book
-    Book book7("Elia", "Mirafiori", "C++ fatto easy", "888-521-123-E");  // test borrow and return book
+    Book book6("Pippo", "Baudo", "C++ fatto easy", "888-521-321-E");    // test borrow and return book
+    Book book7("Mike", "Wasowsky", "C++ fatto easy", "888-521-123-E");  // test borrow and return book
 
     std::cout << "(book1 == book2) -> " << (book1 == book2) << std::endl;
     std::cout << "(book2 == book3) -> " << (book2 == book3) << std::endl;
@@ -21,6 +21,10 @@ int main() {
     std::cout << "book3 check isbn -> " << book3.check_isbn();
     std::cout << "book4 check isbn -> " << book4.check_isbn();
     std::cout << "book5 check isbn -> " << book5.check_isbn();
+    std::cout << "book6 borrow -> " << book6.borrow_book();
+    std::cout << "book6 return -> " << book6.return_book();
+    std::cout << "book7 return -> " << book6.return_book();
+    std::cout << "book7 borrow -> " << book6.borrow_book();
 
     return 0;
 }
